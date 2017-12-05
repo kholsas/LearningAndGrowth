405b8ad321947c1d7a4362dd8377e4ad7484dddf
Kholofelo Maloma <kholofelom@blts.co.za>
2017-12-05 14:32:47 +0200

    added some more components

--- LearnAngular4/MyFirstAngular4App/src/app/app.module.ts
+++ LearnAngular4/MyFirstAngular4App/src/app/app.module.ts
@@ -5,12 +5,18 @@ import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { MovieManagerComponent } from './movie-manager/movie-manager.component';
+import { SideBarMenuComponent } from './side-bar-menu/side-bar-menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    MovieManagerComponent,
+    SideBarMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ LearnAngular4/MyFirstAngular4App/src/app/movie-manager/movie-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-manager',
+  templateUrl: './movie-manager.component.html',
+  styleUrls: ['./movie-manager.component.css']
+})
+export class MovieManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
